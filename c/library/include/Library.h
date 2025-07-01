f87bf106757b1d328ae430c2b52d546f1f445da5
Juyeong Maing <mjy9088@naver.com>
2025-07-01 21:26:25 +0900

    make Library C++ compatible

--- c/library/include/Library.h
+++ c/library/include/Library.h
@@ -1,7 +1,18 @@
-#pragma once
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct Library *Library;
 
 Library LibraryOpen(const char *path);
 void *LibraryGet(Library self, const char *name);
 void LibraryClose(Library self);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
